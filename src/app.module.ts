4519af6b2070cce812861c8fb3d00a351af18cbe
Kivi27 <54037162+Kivi27@users.noreply.github.com>
2023-03-03 02:59:23 +0200

    fix: register in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,19 +7,25 @@ import { TypeOrmExModule } from './typeorm-ex/typeorm-ex.module';
 import { TodoCardRepository } from './repositories/todo-card.repository';
 import { TodoCardController } from './controllers/todo-card/todo-card.controller';
 import { TodoCardService } from './services/todo-card/todo-card.service.';
+import { TodoCardItemController } from './controllers/todo-card-item/todo-card-item.controller';
+import { TodoCardItemService } from './services/todo-card-item/todo-card-item.service';
+import { TodoCardItemRepository } from './repositories/todo-card-item.repository';
 
 const repositories = [
   TodoCardRepository,
+  TodoCardItemRepository,
 ]
 
 const controllers = [
   AppController,
   TodoCardController,
+  TodoCardItemController,
 ]
 
 const providers = [
   AppService,
   TodoCardService,
+  TodoCardItemService,
 ]
 
 @Module({
